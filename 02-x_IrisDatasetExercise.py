9b8784740eca694a67315d69898a073bc17e7350
ValtteriJH <valtteri.huhdankoski@hotmail.com>
2020-08-03 16:49:52 +0300

    Tutorial lines in quotes, data ready for actual algorithms

--- 02-x_IrisDatasetExercise.py
+++ 02-x_IrisDatasetExercise.py
@@ -1,3 +1,4 @@
+'''
 # Check the versions of libraries
 
 # Python versionpip
@@ -47,3 +48,52 @@ dataset = read_csv(url, names=names)
 
 print(dataset)
 print('----------')
+
+# Data summary
+
+# shape
+print(dataset.shape)
+# head
+print(dataset.head(20))
+# descriptions
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+
+# histograms
+dataset.hist()
+pyplot.show()
+# histograms
+dataset.hist()
+pyplot.show()
+
+
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+'''
+
+# visualize the data
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+# histograms
+dataset.hist()
+pyplot.show()
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
